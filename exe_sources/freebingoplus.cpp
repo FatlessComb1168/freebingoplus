537b40b2dec7df13e47dd0f4452bfe5486823e67
FatlessComb1168 <84366597+FatlessComb1168@users.noreply.github.com>
2021-12-30 12:32:03 +0300

    Update freebingoplus.cpp

--- exe_sources/freebingoplus.cpp
+++ exe_sources/freebingoplus.cpp
@@ -1,20 +1,20 @@
 /*
-Reliable Launcher.
+FreeBingo+ Launcher.
 Copyright (C) 2021 by Fedor Egorov <fedoregorov1@yandex.ru>
-This file is part of Reliable.
+This file is part of FreeBingo+.
 
-Reliable is free software: you can redistribute it and/or modify
+FreeBingo+ is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Reliable is distributed in the hope that it will be useful,
+FreeBingo+ is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Reliable. If not, see <https://www.gnu.org/licenses/>.
+along with FreeBingo+. If not, see <https://www.gnu.org/licenses/>.
 */
 
 #include <iostream>
